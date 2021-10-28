533f586ec5703f8eb51af366d90d8998a85705cc
Uyen Tran <utra0001@student.monash.edu>
2021-10-28 23:26:03 +1100

    virtualised filter list for performance

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,21 @@
 import FilterListIcon from "@mui/icons-material/FilterList";
 import {
   Autocomplete,
+  autocompleteClasses,
   Button,
   CircularProgress,
   Collapse,
+  ListSubheader,
   Paper,
+  Popper,
   TextField,
   Typography,
+  useMediaQuery,
   useTheme,
 } from "@mui/material";
+import { styled } from "@mui/styles";
 import React, { FC, useEffect, useState } from "react";
+import { ListChildComponentProps, VariableSizeList } from "react-window";
 import { getCoins, getCoinSimple } from "../api/CoinGeckoApi";
 import CoinList from "../components/CoinList";
 import NavBar from "../components/NavBar";
@@ -106,6 +112,11 @@ const Home: FC = () => {
               }}
               loading={isLoadingCoinSymbols}
               style={{ width: 500 }}
+              disableListWrap
+              PopperComponent={StyledPopper}
+              ListboxComponent={ListboxComponent}
+              renderOption={(props, option) => [props, option]}
+              renderGroup={(params) => params}
               renderInput={(params) => (
                 <TextField
                   {...params}
@@ -167,5 +178,108 @@ const Home: FC = () => {
     </div>
   );
 };
+
+const LISTBOX_PADDING = 8; // px
+
+function renderRow(props: ListChildComponentProps) {
+  const { data, index, style } = props;
+  const dataSet = data[index];
+  const inlineStyle = {
+    ...style,
+    top: (style.top as number) + LISTBOX_PADDING,
+  };
+
+  return (
+    <Typography component="li" {...dataSet[0]} noWrap style={inlineStyle}>
+      {dataSet[1].name}
+    </Typography>
+  );
+}
+
+const OuterElementContext = React.createContext({});
+
+const OuterElementType = React.forwardRef<HTMLDivElement>((props, ref) => {
+  const outerProps = React.useContext(OuterElementContext);
+  return <div ref={ref} {...props} {...outerProps} />;
+});
+
+function useResetCache(data: any) {
+  const ref = React.useRef<VariableSizeList>(null);
+  React.useEffect(() => {
+    if (ref.current != null) {
+      ref.current.resetAfterIndex(0, true);
+    }
+  }, [data]);
+  return ref;
+}
+
+// Adapter for react-window
+const ListboxComponent = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLElement>
+>(function ListboxComponent(props, ref) {
+  const { children, ...other } = props;
+  const itemData: React.ReactChild[] = [];
+  (children as React.ReactChild[]).forEach(
+    (item: React.ReactChild & { children?: React.ReactChild[] }) => {
+      itemData.push(item);
+      itemData.push(...(item.children || []));
+    }
+  );
+
+  const theme = useTheme();
+  const smUp = useMediaQuery(theme.breakpoints.up("sm"), {
+    noSsr: true,
+  });
+  const itemCount = itemData.length;
+  const itemSize = smUp ? 36 : 48;
+
+  const getChildSize = (child: React.ReactChild) => {
+    if (child.hasOwnProperty("group")) {
+      return 48;
+    }
+
+    return itemSize;
+  };
+
+  const getHeight = () => {
+    if (itemCount > 8) {
+      return 8 * itemSize;
+    }
+    return itemData.map(getChildSize).reduce((a, b) => a + b, 0);
+  };
+
+  const gridRef = useResetCache(itemCount);
+
+  return (
+    <div ref={ref}>
+      <OuterElementContext.Provider value={other}>
+        <VariableSizeList
+          itemData={itemData}
+          height={getHeight() + 2 * LISTBOX_PADDING}
+          width="100%"
+          ref={gridRef}
+          outerElementType={OuterElementType}
+          innerElementType="ul"
+          itemSize={(index) => getChildSize(itemData[index])}
+          overscanCount={5}
+          itemCount={itemCount}
+        >
+          {renderRow}
+        </VariableSizeList>
+      </OuterElementContext.Provider>
+    </div>
+  );
+});
+
+const StyledPopper = styled(Popper)({
+  [`& .${autocompleteClasses.listbox}`]: {
+    boxSizing: "border-box",
+    "& ul": {
+      padding: 0,
+      margin: 0,
+    },
+  },
+});
 
 export default Home;
