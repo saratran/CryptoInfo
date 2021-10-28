4f0bf79372be8c454b09ca08e054db2c99bbe24f
Uyen Tran <utra0001@student.monash.edu>
2021-10-29 00:47:25 +1100

    modify basename

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <BrowserRouter
-        basename={process.env.BASENAME ? "" : process.env.BASENAME}
+        basename={process.env.BASENAME ? process.env.BASENAME : "/"}
       >
         <Routes />
       </BrowserRouter>
